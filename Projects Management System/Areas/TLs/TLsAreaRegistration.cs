9c8d32ce3713a6fc7e78943c68039d623f6656d4
ramimohsen <ramimohsen72@yahoo.com>
2017-04-11 02:38:11 +0200

    Areas added

---
+++ Projects Management System/Areas/TLs/TLsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Projects_Management_System.Areas.TLs
+{
+    public class TLsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "TLs";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "TLs_default",
+                "TLs/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
